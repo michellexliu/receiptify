0f9c41760a34eb4b2fc1a8e6ac7728f68a7f1065
tomatopickle <65482369+tomatopickle@users.noreply.github.com>
2022-06-07 12:39:10 +0000

    Refactor: Remove config secrets

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * https://beta.developer.spotify.com/dashboard
  */
 
-const client_id = '89d30ad7388e4d52a61ada58a19c8928';
-const redirect_uri = 'http://localhost:3000/callback';
-const client_secret = 'd7dda67f232541ae8433eb62dbb9df22';
+ const client_id = 'YOUR_CLIENT_ID';
+ const redirect_uri = 'YOUR_CALLBACK_URL';
+ const client_secret = 'YOUR_CLIENT_SECRET';
